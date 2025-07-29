7ede44f9a1cfa1565eb2c7057902908d42f74b95
Deep4054 <deepkayastha4054.com>
2025-07-29 16:13:30 +0530

    New module added for user Work infromation check

--- CommandLineInput/UserWorkInformation.py
+++ CommandLineInput/UserWorkInformation.py
@@ -1,11 +1,17 @@
 
+# User Work information module
+class UserWorkInformation :
+    
 # Counstructor of the module
-def __init__(self):
-    self.userCurrentRole = ""
-    self.userGoal =""
+    def __init__(self):
+        self.userCurrentRole = ""
+        self.userGoal=[]
 
-def SetWorkInformation():
-    while 1:
+    # Work Infromation function
+    def SetWorkInformation(self):
+    
+    # When choice matches the loop terminate
+     while 1:
         print("\n\t1) Student")
         print("\n\t2) Developer")
         print("\n\t3) Content Creator")
@@ -13,7 +19,56 @@ def SetWorkInformation():
         choice = int(input("\n\tPlease enter your choice in number: "))
         
         # constrol statement for user
-        # if choice == 1 :
+        if choice == 1 :
+            self.userCurrentRole = "Student"
+            break
+        elif choice ==2:
+            self.userCurrentRole = "Developer"
+            break
+        elif choice == 3:
+            self.userCurrentRole ="Content Creator"
+            break
+        elif choice == 4:
+            self.userCurrentRole ="Manager"
+            break
+        else :
+            print("\n\tPlease Enter valid choice: ")
+    
+    #goal information setup mathod
+    def SetGoalInformation(self):
+        
+        while 1 :
+            # choice selection
+            print("\n\tPlease Enter choice: ")
+            print("\n\t1) Learner")
+            print("\n\t2) Developer")
+            print("\n\t3) Job oriented")
+            print("\n\t4) Free lanser")
+            print("\n\t5) Exit")
+            choice = int(input("\n\tPlease Enter your choice "))
+            if choice == 1 :
+                self.userGoal.append("Learner");
+            elif choice == 2:
+                self.userGoal.append("Developer");
+            elif choice ==3:
+                self.userGoal.append("Job oriented");
+            elif choice ==4:
+                self.userGoal.append("Free lanser");
+            elif choice ==5:
+                break
+            else:
+                print("\n\tPlease enter valid choice: ")
+# Complete Module With some test cases
+#     def show_info(self):
+#         print("\n\tUser's Current goal: ",self.userGoal)
+#         print("\n\tUser's Current Role: ",self.userCurrentRole)
+
+# user = UserWorkInformation()
+# user.SetWorkInformation()
+# user.SetGoalInformation()
+# user.show_info()
+             
+            
             
 
         
